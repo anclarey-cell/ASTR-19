abdb2660d212a6d957852ca0997f590585513bf3
anclarey-cell <anclarey@ucsc.edu>
2026-04-08 17:54:38 -0700

    Add files via upload
    04/08/2026

---
+++ Session 2 prompt.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:48:33 2026
+
+@author: arian
+"""
+
+x = 8
+y = 4
+
+print ("x-y=", x-y)
+
+
+a = 1.83
+b = 2.95
+
+print ("a+b=", a+b)
+
+
+print ("x*a", x*a)
