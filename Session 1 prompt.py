38ff42b632f7d04bb5e11a7ba6771269084a8e54
anclarey-cell <anclarey@ucsc.edu>
2026-04-08 17:47:35 -0700

    Add files via upload
    04/08/2026

---
+++ Session 1 prompt.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:22:48 2026
+
+@author: arian
+"""
+
+a = "Ariana Noelle Clarey"
+b = "She/Her/Hers"
+c = "My favortie movie has to be Nerve and it basically a giant game of truth or dare without the option of truth. Each dare you complete gets you money and you can stop any time after completed dares. If you fail or bail in the middle of a dare all the money you have earned is taken away. My favorite food is probably pasta, I could eat it for every meal."
+
+
+print (a)
+print (b)
+print (c)
