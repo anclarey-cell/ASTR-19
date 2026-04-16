df0c63c6cb19f31da4fb9c2e7dde8668ad1fb4ce
anclarey-cell <anclarey@ucsc.edu>
2026-04-15 18:20:28 -0700

    Add files via upload
    4/15/2026

---
+++ Session_5_prompt.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 18:11:29 2026
+
+@author: arian
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2*np.pi, 1000)
+print(x)
+
+fig, axs = plt.subplots(nrows=1, ncols=1)
+
+sinx = np.sin(x)
+
+axs.plot(x, sinx, label='sin x')
+axs.set_xlabel("x")
+axs.set_ylabel("y")
