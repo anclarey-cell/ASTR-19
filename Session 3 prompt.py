ceef6f86016487919860b9c58f51dc13f8958585
anclarey-cell <anclarey@ucsc.edu>
2026-04-15 18:09:34 -0700

    Add files via upload
    4/15/2026

---
+++ Session 3 prompt.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:58:14 2026
+
+@author: arian
+"""
+
+
+def f(x):
+    
+    return x**3+8
+
+        
+def main():
+    
+    x = 9
+    print("x**3+8", x**3+8)
+    
+    
+    if f(x) >= 27 :
+        print ("YAY")
+
+        
+if __name__ == "__main__":
+        main()
