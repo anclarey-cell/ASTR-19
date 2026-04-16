50bb6640c18c063dc88d6322a8af0aa8ad9e6d12
anclarey-cell <anclarey@ucsc.edu>
2026-04-15 18:23:02 -0700

    Add files via upload
    4/15/2026

--- Session_5_prompt.py
+++ Session_5_prompt.py
@@ -8,13 +8,18 @@ Created on Wed Apr 15 18:11:29 2026
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 2*np.pi, 1000)
-print(x)
+def main():
+    x = np.linspace(0, 2*np.pi, 1000)
+    print(x)
 
-fig, axs = plt.subplots(nrows=1, ncols=1)
+    fig, axs = plt.subplots(nrows=1, ncols=1)
 
-sinx = np.sin(x)
+    sinx = np.sin(x)
 
-axs.plot(x, sinx, label='sin x')
-axs.set_xlabel("x")
-axs.set_ylabel("y")
+    axs.plot(x, sinx, label='sin x')
+    axs.set_xlabel("x")
+    axs.set_ylabel("y")
+    
+    
+if __name__ == "__main__":
+    main()
